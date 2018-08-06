50fba3f1797c64aacb2e7d5ccee78bf2923d4654
Brian Clincy <bclincy@purplerockscissors.com>
2018-08-06 10:54:55 -0400

    getting symbols data

--- yahoo.js
+++ yahoo.js
@@ -6,16 +6,21 @@ const fs = require('fs-extra');
 
         async function getData (url, selector){
             const browser = await puppeteer.launch({headless: true});
-            const page = await browser.newPage();
+             const page = await browser.newPage();
             await page.goto(url, {waitUntil: 'domcontentloaded'});
             await page.waitForSelector(selector);
-            const data = await page.$$(selector);
-            const link = await page.$x('//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[2]/span[2]/a');
-            const linkData = await page.evaluate( data => data.outerText, data); 
-            console.log(linkData);
-            // return page.content();
-            browser.close;
-            return data[0].outerText;
+            const table = await page.$$(selector);
+            const link = await page.$x('//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[2]/span[2]');
+            //get data and link for data gathering
+            console.log( await table[0].$eval('tbody', tbody => tbody.outerText));
+            console.log( await link[0].$eval('a', a => a.href));
+            // for (const data of table) {
+            //     const tdata = await data.$('tbody');
+            //     console.log(await data.$eval('tbody', tbody => tbody.outerText ));
+            // }
+
+            browser.close();
+            return 'yes I made it';
         }
 
         async function downLoadCsv (downloadUrl) {
@@ -25,12 +30,7 @@ const fs = require('fs-extra');
               }, downloadUrl);
         }
         console.log(await getData('https://finance.yahoo.com/quote/AAPL/history?p=AAPL', '#Col1-1-HistoricalDataTable-Proxy > section > div > table' ));
-        // const browser = await puppeteer.launch({headless: true});
-        // const page = await browser.newPage();
-        // page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36');
 
-        // await page.goto('https://experts.shopify.com');
-        // await page.waitForSelector('.section');
     } catch (e) {
         console.log('our error', e)
     }
